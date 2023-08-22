fbcac298861799f91ea621c9cb3ca398803b3f80
Kethelyn Alves <141461344+KethelynAlves@users.noreply.github.com>
2023-08-21 21:38:56 -0300

    calculo de folha de pagamento VT+INSS
    melhoramento e limpeza do calculo

--- folha de pagamento2.py
+++ folha de pagamento2.py
@@ -2,33 +2,34 @@ sal = float(input("Digite o seu salário bruto: "))
 vale = input("Descontar o vale transporte? ")
 
 
-if sal <1302.00:
-    inss = sal*0.075
-    print (f"Calculo do INSS R${inss}")
+if sal <1320.00:
+    inss = (sal*0.075)
     total = (sal-inss)
-    print ("Seu salário liquído é de:R$", total)
+    print (f"Calculo do INSS R${inss}")
+    print (f"Seu salário liquído é de:R$ {total}")
 elif sal <2571.29:
-    inss = sal*0.09
-    print (f"Calculo do INSS R${inss}")
+    inss = ((sal-1320)*0.09 + 99)
     total = (sal-inss)
-    print ("Seu salário liquído é de:R$", total)
+    print (f"Calculo do INSS R${inss}")
+    print (f"Seu salário liquído é de:R$ {total}")
 elif sal <3856.94:
-    inss = sal*0.12
-    print (f"Calculo do INSS R${inss}")
+    inss = ((sal-2571.29)*0.12 + 112.62 + 99)
     total = (sal-inss)
-    print ("Seu salário liquído é de:R$", total)
+    print (f"Calculo do INSS R${inss}")
+    print (f"Seu salário liquído é de:R$ {total}")
 elif sal <7507.49:
-    inss = sal*0.14
-    print (f"Calculo do INSS R${inss}")
+    inss = ((sal-3856.94)*0.14 + 154.28 + 112.62 + 99)
     total = (sal-inss)
-    print ("Seu salário liquído é de:R$", total)
-elif sal >7507.50:
+    print (f"Calculo do INSS R${inss}")
+    print (f"Seu salário liquído é de:R$ {total}")
+elif sal >=7507.50:
     inss = 876.95
-    print (f"Calculo do INSS R${inss}")
     total = (sal-inss)
-    print ("Seu salário liquído é de:R$", total)
+    print (f"Calculo do INSS R${inss}")
+    print (f"Seu salário liquído é de:R$ {total}")
+
 if vale == "S" or vale == "s":
-    valvale =total*0.06
+    valvale =sal*0.06
     print ("O valor do Vale transporte é de: R$", valvale)
     total = sal-valvale
     print ("Seu salário liquído com desconto do Vale Transporte é de: R$", total)
